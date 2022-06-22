3c499eb74c236a10e1dffbf9de0c756a0f6c3ff2
Sean Carlson <92127265+SeanCarlson96@users.noreply.github.com>
2022-06-22 15:56:52 -0600

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get("/api", (req, res) => {
+    const teamEntered = req.query.q
+    const SerpApi = require('google-search-results-nodejs');
+    const search = new SerpApi.GoogleSearch("91c5436ea5b5576335cf2cc35e27b5c23f03f1b8972220f1d0113f9bd7ce03bb");
+    const params = {
+        q: teamEntered,
+        location: "austin, texas, united states"
+    };
+    const callback = function (response) {
+        res.send(response['sports_results']);
+    };
+    search.json(params, callback);
+          
+          ///////////MongoDB create operation
+          const { MongoClient } = require('mongodb');
+          async function main() {
+            const uri = process.env.MONGODB_URI;
+            const client = new MongoClient(uri);
+            var months = ['Jan', 'Feb', 'Mar', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            now = new Date(),
+            timeAndDateSearched = now.getFullYear() + ' ' + months[now.getMonth()] + ' ' + 
+                now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + 
+                now.getSeconds() + ' ' + "GMT";
+            try {
+              await client.connect();
+              await newDocument(client, {
+                teamEntered: teamEntered,
+                timeAndDateSearched: timeAndDateSearched
+              })
+            } catch (e) {
+              console.error(e);
+            } finally {
+              await client.close();
+            }
+          }
+          main().catch(console.error);
+          ///////////
+          
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+async function newDocument(client, newUser) {
+  const result = await client.db("ttDB").collection("TeamsSearched").insertOne(newUser);
+}
